392e6dab1fe3a9d0df460c0a68a656d8acc8d9b5
Thomas Lang <thomaslang2003@me.com>
2022-10-15 19:25:29 -0500

    saved total

--- Chrome/Query.js
+++ Chrome/Query.js
@@ -1,11 +1,18 @@
+let total = 0;
+
+chrome.storage.sync.get(["totalSaved"], function (result) {
+  if (result != null && result.totalSaved != undefined) {
+    console.log("Value currently is " + result.totalSaved);
+    total = result.totalSaved;
+    document.getElementById("total").innerHTML = total;
+  }
+});
+
 function updateScore() {
   let value = document.getElementById("amount").value;
-  let mcc = document.querySelector('input[name = mcc]:checked').value;
-  console.log("mcc:"+mcc);  
-  console.log(value);
+  let mcc = document.querySelector("input[name = mcc]:checked").value;
 
-
-  data = {"money": value, "mcc": mcc};
+  data = { money: value, mcc: mcc };
   fetch("http://localhost:8080", {
     method: "POST",
     body: JSON.stringify(data),
@@ -15,13 +22,32 @@ function updateScore() {
   })
     .then((res) => res.json())
     .then((jsonic) => {
-      text = jsonic.transactionFootprints[0].carbonEmissionInOunces;
-      console.log(text, value);
+      console.log(total);
+      text = parseFloat(jsonic.transactionFootprints[0].carbonEmissionInOunces);
+      total += text;
+
+      console.log(text);
+      console.log(total);
+
       document.getElementById("impact").innerHTML = text;
+
+      document.getElementById("total").innerHTML = total;
+
+      chrome.storage.sync.set({ totalSaved: total }, function () {
+        console.log("Value is set to " + total);
+      });
     })
     .catch((err) => console.log(err));
 }
 
-document
-  .getElementById("submit")
-  .addEventListener("click", updateScore, false);
+function resetScore() {
+  total = 0;
+  document.getElementById("total").innerHTML = total;
+
+  chrome.storage.sync.set({ totalSaved: total }, function () {
+    console.log("Value is set to " + total);
+  });
+}
+
+document.getElementById("submit").addEventListener("click", updateScore, false);
+document.getElementById("reset").addEventListener("click", resetScore);
