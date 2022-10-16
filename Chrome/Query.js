caeb7e151991ecbc2fd88daa488a2b8d3d2e1768
GeorgeLuo2025 <l.xiangsheng@wustl.edu>
2022-10-15 20:25:54 -0500

    j

--- Chrome/Query.js
+++ Chrome/Query.js
@@ -12,16 +12,49 @@ chrome.storage.sync.get(["totalSaved"], function (result) {
 function updateImage() {
   //change image according to value
   if (total > 1000) {
-    document.getElementById("anime").src = "";
+    document.getElementById("anime").src = "verysick_cactus.gif";
   } else if (total > 750) {
-    document.getElementById("anime").src = "";
+    document.getElementById("anime").src = "sick_cactus.gif";
   } else if (total > 500) {
-    document.getElementById("anime").src = "";
+    document.getElementById("anime").src = "main_cactus.gif";
   } else if (total > 250) {
-    document.getElementById("anime").src = "";
+    document.getElementById("anime").src = "happy_cactus.gif";
   } else {
-    document.getElementById("anime").src = "";
+    document.getElementById("anime").src = "veryhappy_cactus.gif";
   }
+}
+
+function previewScore() {
+  let value = document.getElementById("amount").value;
+  let mcc = document.querySelector("input[name = mcc]:checked").value;
+
+  data = { money: value, mcc: mcc };
+  fetch("http://localhost:8080", {
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: {
+      "content-type": "application/json",
+    },
+  })
+    .then((res) => res.json())
+    .then((jsonic) => {
+      console.log(total);
+      text = parseFloat(jsonic.transactionFootprints[0].carbonEmissionInOunces);
+    //  total += text;
+
+      console.log(text);
+    //  console.log(total);
+
+      document.getElementById("impact").innerHTML = text;
+
+    //  document.getElementById("total").innerHTML = total;
+
+    //  chrome.storage.sync.set({ totalSaved: total }, function () {
+    //    console.log("Value is set to " + total);
+    //  });
+    //  updateImage();
+    })
+    .catch((err) => console.log(err));
 }
 
 function updateScore() {
